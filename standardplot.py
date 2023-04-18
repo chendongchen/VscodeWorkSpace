ff65a494c6086d914910b0b9131991154bfd224b
chendongchen <3242986122@qq.com>
2023-04-19 03:45:08 +0800

    maploltib

--- standardplot.py
+++ standardplot.py
@@ -3,4 +3,4 @@ import numpy as np
 
 x = np.linspace(0, 200, 100)  # Create a list of evenly-spaced numbers over the range
 plt.plot(x, np.sin(x))       # Plot the sine of each x point
-plt.show()                   # Display the plotp
+plt.show()                  # Display 
