a743a9119829370d4340a734d44d194f1246374a
chendongchen <3242986122@qq.com>
2023-04-27 23:32:59 +0800

    CMake在Vscode其他工作目录的使用

---
+++ build/CMakeFiles/ExampleProject.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ExampleProject.dir/main.cpp.obj"
+  "CMakeFiles/ExampleProject.dir/main.cpp.obj.d"
+  "ExampleProject.exe"
+  "ExampleProject.exe.manifest"
+  "ExampleProject.pdb"
+  "libExampleProject.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ExampleProject.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
